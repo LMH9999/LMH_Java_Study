80ab1003577d50085bff317547c16b72b2dddf39
LMH <leemh9256@gmail.com>
2023-10-11 14:21:20 +0900

    파이널 필드와 상수 선언 실습

---
+++ myclass/exam04/Earth.java
@@ -0,0 +1,11 @@
+package com.mystudy.myclass.exam04;
+
+//파이널 필드 상수
+public class Earth {
+    static final double EARTH_RADIUS = 6400;
+    static final double EARTH_SURFACE_AREA;
+
+    static {
+        EARTH_SURFACE_AREA = 4 * Math.PI * EARTH_RADIUS * EARTH_RADIUS;
+    }
+}
